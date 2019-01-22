f315ffccbdb64b2463c3a3dd829a96bd608874d1
tharaniramesh <46441125+tharaniramesh@users.noreply.github.com>
2019-01-22 08:52:34 -0800

    Create isalpha_numeric.py

---
+++ isalpha_numeric.py
@@ -0,0 +1,13 @@
+s=input()
+#print
+c=0
+d=0
+for i in range(len(s)):
+    if s[i].isdigit():
+        c=c+1
+    elif s[i].isalpha():
+        d=d+1
+if c>0 and d>0:
+    print("Yes")
+else:
+    print("No")
